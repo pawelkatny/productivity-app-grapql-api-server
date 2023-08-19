f77dab8d4df6e15a6a397e59b9c57e2b338cd239
Paweł Kątny <pawel.katny@gmail.com>
2023-08-19 21:58:25 +0200

    Add template typeDefs and resolvers with schema loader for user

---
+++ src/components/user/index.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const { GraphQLFileLoader } = require("@graphql-tools/graphql-file-loader");
+const { loadSchemaSync } = require("@graphql-tools/load");
+const resolvers = require("./resolvers");
+
+const typeDefs = loadSchemaSync(path.resolve(__dirname, "types.graphql"), {
+  loaders: [new GraphQLFileLoader()],
+});
+
+module.exports = {
+  resolvers,
+  typeDefs,
+};
---
+++ src/components/user/resolvers.js
@@ -0,0 +1,7 @@
+module.exports = {
+  Query: {
+    user: async (parent, args, context, info) => {
+      return "user query";
+    },
+  },
+};
