2eadc397cc3a3dbea7c0b16742e061c11708a266
Paweł Kątny <pawel.katny@gmail.com>
2023-08-20 18:47:13 +0200

    Add user schema pre 'save' action to store hashed password

--- src/components/user/model.js
+++ src/components/user/model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const { BCRYPT_SALT } = require("../../config");
 
 const userSchema = mongoose.Schema(
   {
@@ -61,6 +63,14 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function () {
+  const updatedFields = this.modifiedPaths();
+  if (updatedFields.includes("password")) {
+    const salt = await bcrypt.salt(BCRYPT_SALT);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+});
 
 const User = mongoose.model("User", userSchema);
 
