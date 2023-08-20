8ab670c8929df68652e6ac4e324562e8b8ee9c27
Paweł Kątny <pawel.katny@gmail.com>
2023-08-20 16:48:09 +0200

    Update executable schema loader with user typeDefs and resolvers

--- src/loaders/schema.js
+++ src/loaders/schema.js
@@ -3,8 +3,12 @@ const {
   typeDefs: taskTypeDefs,
   resolvers: taskResolvers,
 } = require("../components/task");
+const {
+  typeDefs: userTypeDefs,
+  resolvers: userResolvers,
+} = require("../components/user");
 
 module.exports = makeExecutableSchema({
-  typeDefs: [taskTypeDefs],
-  resolvers: [taskResolvers],
+  typeDefs: [taskTypeDefs, userTypeDefs],
+  resolvers: [taskResolvers, userResolvers],
 });
