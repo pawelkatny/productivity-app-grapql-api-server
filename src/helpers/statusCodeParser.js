2e792896a6661cc0e05b7076026475d26e0dd66b
Paweł Kątny <pawel.katny@gmail.com>
2023-08-27 19:00:43 +0200

    Add http status code parser to graphql alike codes

---
+++ src/helpers/statusCodeParser.js
@@ -0,0 +1,10 @@
+const { getReasonPhrase } = require("http-status-codes");
+
+const parseStatusCode = (code) => {
+  return getReasonPhrase(code)
+    .replace(/+*?^$()[]{}|\`\'/, "")
+    .replace("/s/", "_")
+    .toUpperCase();
+};
+
+module.exports = parseStatusCode;
