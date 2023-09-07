6d5ce0b9418d4b5d5920b6910ecf535a74d2dfc7
Paweł Kątny <pawel.katny@gmail.com>
2023-09-07 20:46:36 +0200

    Update error handler for mongo validation errors

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -6,7 +6,11 @@ const { expressMiddleware } = require("@apollo/server/express4");
 const {
   ApolloServerPluginDrainHttpServer,
 } = require("@apollo/server/plugin/drainHttpServer");
-const { ApolloServerErrorCode } = require("@apollo/server/errors");
+const {
+  ApolloServerErrorCode,
+  unwrapResolverError,
+} = require("@apollo/server/errors");
+const { ValidationError } = require("mongoose").Error;
 const { StatusCodes } = require("http-status-codes");
 const express = require("express");
 const http = require("http");
@@ -20,16 +24,30 @@ const server = new ApolloServer({
   schema,
   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   formatError: (formattedError, error) => {
-    if (
-      formattedError.extensions.code ===
-      ApolloServerErrorCode.INTERNAL_SERVER_ERROR
-    ) {
-      error.extensions.http = {
+    const { message, extensions } = formattedError;
+    const originalError = unwrapResolverError(error);
+    let errorsPretty = [];
+
+    if (extensions.code === ApolloServerErrorCode.INTERNAL_SERVER_ERROR) {
+      extensions.http = {
         status: StatusCodes.INTERNAL_SERVER_ERROR,
       };
     }
 
-    return { ...formattedError, http: { ...error.extensions.http } };
+    if (originalError instanceof ValidationError) {
+      for (const err in originalError.errors) {
+        errorsPretty.push({
+          message: originalError.errors[err].message,
+          path: originalError.errors[err].path,
+        });
+      }
+      extensions.code = ApolloServerErrorCode.BAD_USER_INPUT;
+      extensions.http = {
+        status: StatusCodes.BAD_REQUEST,
+      };
+    }
+
+    return { message, errorsPretty, extensions };
   },
 });
 
