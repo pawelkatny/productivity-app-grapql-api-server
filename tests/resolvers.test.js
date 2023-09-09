5085f9c792fd19776ca627ef84e1bae105877e5c
Paweł Kątny <pawel.katny@gmail.com>
2023-09-09 21:38:07 +0200

    Group tests for registerUser resolver

--- tests/resolvers.test.js
+++ tests/resolvers.test.js
@@ -21,78 +21,80 @@ afterAll(async () => {
 });
 
 describe("User resolver", () => {
-  it("should register user and return auth token data", async () => {
-    const contextValue = {
-      db: context.db,
-    };
+  describe("registerUser", () => {
+    it("should register user and return auth token data", async () => {
+      const contextValue = {
+        db: context.db,
+      };
 
-    const mockUserInputData = {
-      email: "test@email.com",
-      name: "Johny",
-      password: "paS$w0rd",
-    };
+      const mockUserInputData = {
+        email: "test@email.com",
+        name: "Johny",
+        password: "paS$w0rd",
+      };
 
-    const res = await server.executeOperation(
-      {
-        query:
-          "mutation Mutation($input: RegisterUserInput!) {registerUser(input: $input) {name}}",
-        variables: {
-          input: mockUserInputData,
+      const res = await server.executeOperation(
+        {
+          query:
+            "mutation Mutation($input: RegisterUserInput!) {registerUser(input: $input) {name}}",
+          variables: {
+            input: mockUserInputData,
+          },
         },
-      },
-      {
-        contextValue,
-      }
-    );
+        {
+          contextValue,
+        }
+      );
 
-    expect(res.body.singleResult.errors).toBeUndefined();
-    expect(res.body.singleResult.data).toBeDefined();
-    expect(res.body.singleResult.data.registerUser).toEqual({
-      name: mockUserInputData.name,
+      expect(res.body.singleResult.errors).toBeUndefined();
+      expect(res.body.singleResult.data).toBeDefined();
+      expect(res.body.singleResult.data.registerUser).toEqual({
+        name: mockUserInputData.name,
+      });
     });
-  });
 
-  it("should throw validation error on password not matching requirements", async () => {
-    const contextValue = {
-      db: context.db,
-    };
+    it("should throw validation error on password not matching requirements", async () => {
+      const contextValue = {
+        db: context.db,
+      };
 
-    const mockUserInputIncorrectData = {
-      email: "testAnother@email.com",
-      name: "Johny",
-      password: "password",
-    };
+      const mockUserInputIncorrectData = {
+        email: "testAnother@email.com",
+        name: "Johny",
+        password: "password",
+      };
 
-    const res = await server.executeOperation(
-      {
-        query:
-          "mutation Mutation($input: RegisterUserInput!) {registerUser(input: $input) {name}}",
-        variables: {
-          input: mockUserInputIncorrectData,
+      const res = await server.executeOperation(
+        {
+          query:
+            "mutation Mutation($input: RegisterUserInput!) {registerUser(input: $input) {name}}",
+          variables: {
+            input: mockUserInputIncorrectData,
+          },
         },
-      },
-      {
-        contextValue,
-      }
-    );
+        {
+          contextValue,
+        }
+      );
 
-    expect(res.body.singleResult.data).toBeNull();
-    expect(res.body.singleResult.errors).toBeDefined();
-    expect(res.body.singleResult.errors).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          errorsPretty: [
-            {
-              message:
-                "Password should contain at least: one uppercase character, one lowercase character, one digit and one special character (@$!%*?&).",
-              path: "password",
-            },
-          ],
-        }),
-      ])
-    );
-    expect(res.body.singleResult.errors[0].extensions.code).toEqual(
-      ApolloServerErrorCode.BAD_REQUEST
-    );
+      expect(res.body.singleResult.data).toBeNull();
+      expect(res.body.singleResult.errors).toBeDefined();
+      expect(res.body.singleResult.errors).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            errorsPretty: [
+              {
+                message:
+                  "Password should contain at least: one uppercase character, one lowercase character, one digit and one special character (@$!%*?&).",
+                path: "password",
+              },
+            ],
+          }),
+        ])
+      );
+      expect(res.body.singleResult.errors[0].extensions.code).toEqual(
+        ApolloServerErrorCode.BAD_REQUEST
+      );
+    });
   });
 });
