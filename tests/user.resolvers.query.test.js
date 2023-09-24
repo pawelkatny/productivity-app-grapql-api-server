dd907b9ae4604a909f39ff1a6bab9970714ca45a
Paweł Kątny <pawel.katny@gmail.com>
2023-09-24 18:30:15 +0200

    Add user resolver query test

--- tests/user.resolvers.query.test.js
+++ tests/user.resolvers.query.test.js
@@ -2,8 +2,8 @@ const apolloServer = require("../src/helpers/mockApolloServer");
 const dbServer = require("../src/helpers/mockDbServer");
 const context = require("../src/context");
 const { ApolloServerErrorCode } = require("@apollo/server/errors");
-const { jwt } = require("../src/helpers");
-const { StatusCodes } = require("http-status-codes");
+const { jwt, parseStatusCode } = require("../src/helpers");
+const { StatusCodes, getReasonPhrase } = require("http-status-codes");
 
 let server;
 
@@ -58,6 +58,56 @@ describe("User resolver queries", () => {
 
       expect(res.body.singleResult.data.getUser).toEqual(mockUserObjectData);
       expect(res.body.singleResult.errors).toBeUndefined();
+    });
+
+    it("should throw error if user is not auth", async () => {
+      const { User } = context.db;
+      const user = new User(mockUserInputData);
+      const contextValue = {
+        db: context.db,
+        authUser: false,
+      };
+
+      const res = await server.executeOperation(
+        {
+          query: `query Query { getUser { name, settings { defaultView, taskRequestLimit } lastLoginDate }}`,
+        },
+        {
+          contextValue,
+        }
+      );
+
+      expect(res.body.singleResult.data).toBeNull();
+      expect(res.body.singleResult.errors).toBeDefined();
+    });
+
+    it("should throw error if user is not found", async () => {
+      const { User } = context.db;
+      const user = null;
+      const contextValue = {
+        db: context.db,
+        authUser: true,
+      };
+
+      jest.spyOn(User, "findById").mockImplementationOnce(() => user);
+
+      const res = await server.executeOperation(
+        {
+          query: `query Query { getUser { name, settings { defaultView, taskRequestLimit } lastLoginDate }}`,
+        },
+        {
+          contextValue,
+        }
+      );
+
+      expect(res.body.singleResult.data).toBeNull();
+      expect(res.body.singleResult.errors).toBeDefined();
+      expect(res.body.singleResult.errors[0].message).toEqual(
+        getReasonPhrase(StatusCodes.NOT_FOUND)
+      );
+      expect(res.body.singleResult.errors[0].extensions.http.status).toEqual(
+        StatusCodes.NOT_FOUND
+      );
     });
   });
 });
