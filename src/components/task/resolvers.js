371977f91a2e19f0c7d81bf86017994229303be2
Paweł Kątny <pawel.katny@gmail.com>
2023-09-29 22:12:40 +0200

    Add task mutation: createTask

--- src/components/task/resolvers.js
+++ src/components/task/resolvers.js
@@ -1,7 +1,29 @@
+const { StatusCodes } = require("http-status-codes");
+const CustomGraphQLerror = require("../../error/customError");
+
 module.exports = {
   Query: {
     task: async (parent, args, context, info) => {
       return "task query";
+    },
+  },
+  Mutation: {
+    createTask: async (parent, { input }, { authUser, db: { Task } }, info) => {
+      if (!authUser) {
+        throw new CustomGraphQLerror(StatusCodes.UNAUTHORIZED);
+      }
+
+      const task = await Task.create({ ...input, user: authUser.userId });
+
+      if (!task) {
+        throw new CustomGraphQLerror(StatusCodes.INTERNAL_SERVER_ERROR);
+      }
+
+      return {
+        ...task._doc,
+        id: task._doc._id.toString(),
+        date: task._doc.date.toISOString(),
+      };
     },
   },
 };
