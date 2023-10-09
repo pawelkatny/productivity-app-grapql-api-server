fce4147af42011da15f67c35163f3cc5d225b624
Paweł Kątny <pawel.katny@gmail.com>
2023-10-09 22:15:07 +0200

    Add task model static method: getSingleList

--- src/components/task/model.js
+++ src/components/task/model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { prepareTaskTypeObject } = require("../../helpers");
 
 const taskSchema = mongoose.Schema(
   {
@@ -52,6 +53,46 @@ const taskSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+taskSchema.statics.getSingleList = async (params, authUser) => {
+  const { type, start, end, page } = params;
+  const { userId, settings: userSettings } = authUser;
+  let dateStart, endStart;
+
+  dateStart = new Date(start);
+  endStart = new Date(end);
+
+  if (start == end) endStart = endStart.setDate(endStart.getDate() + 1);
+  if (type == "year") {
+    const year = dateStart.getYear();
+    dateStart = new Date(year, 0).toDateString();
+    endStart = new Date(+year + 1, 0).toDateString();
+  }
+
+  const searchParams = {
+    user: userId,
+    type,
+    date: {
+      $gte: dateStart,
+      $lt: endStart,
+    },
+  };
+
+  const tasksCount = await this.countDocuments(searchParams);
+  const tasks = await this.find(searchParams)
+    .skip(page - 1)
+    .limit(userSettings.taskRequestLimit)
+    .sort({ priority: "asc" });
+  const nextPage =
+    page * userSettings.taskRequestLimit >= tasksCount ? 0 : page + 1;
+  const tasksMapped = tasks.map((task) => prepareTaskTypeObject(task));
+
+  return {
+    tasks: tasksMapped,
+    count: tasksCount,
+    nextPage,
+  };
+};
 
 const Task = mongoose.model("Task", taskSchema);
 
