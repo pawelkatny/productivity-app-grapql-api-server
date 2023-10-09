2637d72aa507b0ee4ff06f79768cd5f00dac97db
Paweł Kątny <pawel.katny@gmail.com>
2023-10-09 22:04:50 +0200

    Add helper export prepareTaskTypeObject

--- src/components/task/resolvers.js
+++ src/components/task/resolvers.js
@@ -1,15 +1,6 @@
 const { StatusCodes } = require("http-status-codes");
 const CustomGraphQLerror = require("../../error/customError");
-
-const prepareTaskTypeObject = (task) => {
-  return {
-    ...task._doc,
-    id: task._doc._id.toString(),
-    date: task._doc.date.toISOString(),
-    createdAt: task._doc.createdAt.toISOString(),
-    updatedAt: task._doc.updatedAt.toISOString(),
-  };
-};
+const { prepareTaskTypeObject } = require("../../helpers");
 
 module.exports = {
   Query: {
