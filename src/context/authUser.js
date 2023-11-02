44975259eb8b1e1352041af299f7dc5fc491b0ed
Paweł Kątny <pawel.katny@gmail.com>
2023-11-02 20:09:01 +0100

    Update auth context
    Added validation for specific query: if user is loging in or registering, skip the authorization process

--- src/context/authUser.js
+++ src/context/authUser.js
@@ -1,9 +1,16 @@
 const { JWT_SECRET } = require("../config");
 const { jwt } = require("../helpers");
 const User = require("../components/user/model");
+const CustomGraphQLerror = require("../../error/customError");
 
 const authUser = async (req) => {
   const authorization = req.headers.authorization;
+  const isUserLogingOrRegistering = ["loginUserr", "registerUser"].some((q) =>
+    req.body.query.includes(q)
+  );
+
+  //skip the authorization process if user is loging in or registering
+  if (isUserLogingOrRegistering) null;
 
   if (!authorization || !authorization.startsWith("Bearer")) {
     return null;
