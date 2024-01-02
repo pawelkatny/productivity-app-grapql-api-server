4e8d49e9542ec31fc7f7c26a42ff7764e27042e9
Paweł Kątny <pawel.katny@gmail.com>
2024-01-02 12:35:04 +0100

    Add redis config parsing from ENV

--- src/config/index.js
+++ src/config/index.js
@@ -14,4 +14,7 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRECT,
   JWT_EXPIRATION: process.env.JWT_EXPIRATION,
   JWT_TYPE: process.env.JWT_TYPE,
+  REDIS_URL: process.env.REDIS_URL || "localhost",
+  REDIS_URL: process.env.REDIS_PORT || 6379,
+  REDIS_TKN_BLIST_SET: process.env.REDIS_TKN_BLIST_SET || "token_blacklist",
 };
