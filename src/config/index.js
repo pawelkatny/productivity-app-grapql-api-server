a571ab7a3fe76c5b328d96d21af5097d23b9cef5
Paweł Kątny <pawel.katny@gmail.com>
2024-01-02 14:22:14 +0100

    Update redis config

--- src/config/index.js
+++ src/config/index.js
@@ -15,6 +15,7 @@ module.exports = {
   JWT_EXPIRATION: process.env.JWT_EXPIRATION,
   JWT_TYPE: process.env.JWT_TYPE,
   REDIS_URL: process.env.REDIS_URL || "localhost",
-  REDIS_URL: process.env.REDIS_PORT || 6379,
+  REDIS_PORT: process.env.REDIS_PORT || 6379,
   REDIS_TKN_BLIST_SET: process.env.REDIS_TKN_BLIST_SET || "token_blacklist",
+  REDIS_DB_NUMBER: process.env.REDIS_DB_NUMBER || 0,
 };
