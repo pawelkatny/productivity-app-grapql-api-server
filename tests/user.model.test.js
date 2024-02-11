1a5f6400bdf3fe25e2c844e529ab6616073a15cc
Paweł Kątny <pawel.katny@gmail.com>
2024-02-11 13:19:03 +0100

    Move user model test to test folder

--- tests/user.model.test.js
+++ tests/user.model.test.js
@@ -1,10 +1,10 @@
-const dbServer = require("../../helpers/mockDbServer");
+const dbServer = require("../tests_helpers/mockDbServer");
 const bcrypt = require("bcryptjs");
-const User = require("./model");
-const { jwt } = require("../../helpers");
-const { BCRYPT_SALT, JWT_SECRET, JWT_EXPIRATION } = require("../../config");
+const User = require("../src/components/user/model");
+const { jwt } = require("../src/helpers");
+const { BCRYPT_SALT, JWT_SECRET, JWT_EXPIRATION } = require("../src/config");
 
-jest.mock("../../config", () => ({
+jest.mock("../src/config", () => ({
   BCRYPT_SALT: 10,
   JWT_SECRET: "secret",
   JWT_EXPIRATION: 3600,
