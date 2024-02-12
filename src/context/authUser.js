b1240f26e80e245d46f20531649d5316db00d1c9
Paweł Kątny <pawel.katny@gmail.com>
2024-02-12 22:46:21 +0100

    Update context auth
    Add condition when user is trying to log in with valid token

--- src/context/authUser.js
+++ src/context/authUser.js
@@ -6,24 +6,41 @@ const CustomGraphQLerror = require("../error/customError");
 const redisClient = require("../loaders/redis");
 
 const authUser = async (req) => {
-  const NON_AUTH_QUERIES = ["loginUser", "registerUser", "IntrospectionQuery"];
+  const INTROSPECTION_QUERY = "IntrospectionQuery";
+  const NON_AUTH_QUERIES = ["loginUser", "registerUser"];
   const authorization = req.headers.authorization;
+  const isIntrospectionQuery = req.body.query.includes(INTROSPECTION_QUERY);
   const isUserLogingOrRegistering = NON_AUTH_QUERIES.some((q) =>
     req.body.query.includes(q)
   );
 
-  //skip the authentication process if user is loging in or registering
-  if (isUserLogingOrRegistering) return null;
+  if (isIntrospectionQuery) {
+    return null;
+  }
 
-  if (!authorization || !authorization.startsWith("Bearer")) {
+  if (
+    (!authorization || !authorization.startsWith("Bearer")) &&
+    isUserLogingOrRegistering
+  ) {
+    return null;
+  }
+
+  if (
+    (!authorization || !authorization.startsWith("Bearer")) &&
+    !isUserLogingOrRegistering
+  ) {
     throw new CustomGraphQLerror(StatusCodes.UNAUTHORIZED);
   }
 
   const token = authorization.split(" ")[1];
   const decoded = await jwt.verify(token, JWT_SECRET);
 
-  if (!decoded) {
+  if (!decoded && !isUserLogingOrRegistering) {
     throw new CustomGraphQLerror(StatusCodes.UNAUTHORIZED);
+  }
+
+  if (!decoded && isUserLogingOrRegistering) {
+    return null;
   }
 
   const { userId } = decoded;
@@ -34,6 +51,10 @@ const authUser = async (req) => {
   }
 
   const user = await User.findOne({ _id: userId });
+
+  if (!user && isUserLogingOrRegistering) {
+    return null;
+  }
 
   if (!user) {
     throw new CustomGraphQLerror(StatusCodes.UNAUTHORIZED);
